b7d72efa0e41d3b8d3bc8b5363679187a7ab9cbb
tangkaiyang <945541696@qq.com>
2019-03-22 21:17:28 +0800

    完结,等待二刷

---
+++ chapter13/aiohttp_spider.py
@@ -0,0 +1,94 @@
+# asyncio爬虫,去重,入库,
+# aiomysql库, aiomysql, pyquery
+# 安装出错访问:http://www.lfd.uci.edu/~gohlke/pythonlibs/
+import asyncio
+import re
+
+import aiohttp
+import aiomysql
+from pyquery import PyQuery
+
+
+stopping = False
+start_url = "http://www.jobbole.com/"
+waitting_urls = []
+seen_urls = set()  # 布隆过滤器,set耗费内存大
+
+
+
+
+async def fetch(url):
+    try:
+        async with session.get(url) as resp:
+            print("url status: {}".format(resp.status))
+            if resp.status in [200, 201]:
+                data = await resp.text()
+                return data
+    except Exception as e:
+        print(e)
+
+
+
+def extract_urls(html):
+    urls = []
+    pq = PyQuery(html)
+    for link in pq.items("a"):
+        url = link.attr("href")
+        if url and url.startwith("http") and url not in seen_urls:
+            urls.append(url)
+            waitting_urls.append(url)
+    return urls
+
+
+async def init_urls(url, session):
+    html = await fetch(url, session)
+    seen_urls.add(url)
+    extract_urls(html)
+async def article_handler(url, session, pool):
+    # 获取文章详情并解析入库
+    html = await fetch(url, session)
+    seen_urls.add(url)
+    extract_urls(html)
+    pq = PyQuery(html)
+    title = pq("title").text()
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await conn.execute("SELECT 42;")
+            insert_sql = "insert into article_test(title) values('{}')".format(title)
+            await cur.execute(insert_sql)
+
+
+
+async def consumer(pool):
+    async with aiohttp.ClientSession() as session:
+        while not stopping:
+            if len(waitting_urls) == 0:
+                await asyncio.sleep(0.5)
+                continue
+            url = waitting_urls.pop()
+            print("start get url: {}".format(url))
+            if re.match('http://.*?jobble.com/\d+/', url):
+                if url not in seen_urls:
+                    asyncio.ensure_future(article_handler(url, session, pool))
+            else:
+                if url not in seen_urls:
+                    asyncio.ensure_future(init_urls(url, session))
+
+
+
+async def main(loop):
+    # 等待mysql连接建立好
+    pool = await aiomysql.create_pool(host='127.0.0.1', port=3306,
+                                      user='root', password='root',
+                                      db='aiomysql_test', loop=loop,
+                                      charset="utf8", autocommit=True)
+    async with aiohttp.ClientSession() as session:
+        asyncio.ensure_future(init_urls(start_url, session))
+        asyncio.ensure_future(consumer(pool))
+
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    asyncio.ensure_future(main(loop))
+    loop.run_forever()
